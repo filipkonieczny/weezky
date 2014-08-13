2d2e3aa5a3433b88b7e66ca372a111bbb2766450
jhgrng <jhgrng@gmail.com>
2014-08-13 19:21:04 +0200

    Code refactor + added full commands documentation.

--- commands.py
+++ commands.py
@@ -3,19 +3,28 @@
 
 
 # description
+# commands module that contains all commands that weezky reacts to:
+# !rejoin - rejoin a channel after being kicked, say something
+# !uptime - get info about the uptime
+# !gif <tag(s)> - get a gif with given tag(s)
+# !join <channel(s)> - join given channel(s)
+# !part <channel(s)> - part given channel(s)
+# !quote - get a random quote from quotes.json
 
 
 # imports
 import urllib, json
 
 
-# settings
-
-
 # functions
 def rejoin(socket, channel, msg=""):
-    # TODO: documentation
-    '''
+    '''Rejoin a channel after being kicked and say something.
+
+    (socket, str, str) -> str
+
+    >>> rejoin(s, '#weezky', 'Dude, where's my car?!')
+    'PRIVMSG #weezky :Dude, where\'s my car?!\r\n'
+
     '''
 
     socket.send('JOIN %s\r\n' % channel)
@@ -25,8 +34,14 @@ def rejoin(socket, channel, msg=""):
 
 
 def format_time(time):
-    # TODO: documentation
-    '''
+    '''Get number of seconds from time.time() and format it nicely.
+    Takes a float, returns a dict with formatted time.
+
+    (float) -> dict
+
+    >>> format_time(86461.0)
+    {'days': 1, 'hours': 0, 'minutes': 1, 'seconds': 1}
+
     '''
 
     uptime = {}
@@ -53,10 +68,15 @@ def format_time(time):
 
 
 def uptime(channel, uptime):
-    # TODO: documentation
-    '''
-    '''
+    '''Returns a message with current uptime.
+    Uses 'format_time()' function to easily convert float to dict.
 
+    (str, float) -> str
+
+    >>> uptime('#weezky', 86461.0)
+    'PRIVMSG #weezky :Current uptime: 1 days, 0 hours, 1 minutes and 1 seconds.\r\n'
+
+    '''
 
     uptime = format_time(uptime)
     msg = "Current uptime: %s days, %s hours, %s minutes and %s seconds." % (uptime['days'],
@@ -68,11 +88,9 @@ def uptime(channel, uptime):
 
 
 def gif(channel, tags):
-    # TODO: documentation
-    '''This function collects a gif with a given tag(s).
+    '''Collects a gif with given tag(s).
 
-    ([list of strings]) -> url
-
+    ([list of strings]) -> str
 
     >>> get_gif()
     "No tags given!"
@@ -88,11 +106,9 @@ def gif(channel, tags):
 
     '''
 
-
     # corner case - if there are no tags
     if len(tags) == 0:
         return 'PRIVMSG %s :%s\r\n' % (channel, "There are no tags!")
-
 
     print "tags:", tags
 
@@ -106,50 +122,56 @@ def gif(channel, tags):
         if i < len(tags) - 1:
             url_destination += "+"
 
-
     # load data for given url destination
     data = json.loads(urllib.urlopen(url_destination).read())
-
-    print "data: ", data
 
     # if there is no such gif
     if len(data["data"]) == 0:
         return 'PRIVMSG %s :%s\r\n' % (channel, "No such gif!")
 
-
     # return the link to a randomly selected gif(taking into account tags)
-    # print json.dumps(data, sort_keys=True, indent=4)
     data = data["data"]["image_original_url"]
-
     return 'PRIVMSG %s :%s\r\n' % (channel, data)
 
 
 def join(s, channels):
-    # TODO: documentation
-    # TODO: check if user is already there, if is then:
-    # TODO: display something like: "I'm already there, silly!"
-    '''
-    '''
+    '''Join channel(s).
 
+    (socket, [list of strings]) -> socket.send()
+
+    >>> join(s, ['#weezky1', '#weezky2'])
+    s.send('JOIN #weezky1\r\n')
+    s.send('JOIN #weezky2\r\n')
+
+    '''
 
     for i in channels:
         s.send('JOIN %s\r\n' % i)
 
 
 def part(s, channels):
-    # TODO: documentation
-    '''
-    '''
+    '''Part(leave) channel(s).
 
+    (socket, [list of strings]) -> socket.send()
+
+    >>> leave(s, ['#weezky1', '#weezky2'])
+    s.send('PART #weezky1\r\n')
+    s.send('PART #weezky2\r\n')
+
+    '''
 
     for i in channels:
         s.send('PART %s\r\n' % i)
 
 
 def quote(channel, msg):
-    # TODO: documentation
-    '''
-    '''
+    '''Display a random quote from quotes.json.
 
+    (str, str) -> str
+
+    >>> quote('#weezky', 'Are we there yet?')
+    'PRIVMSG #weezky :Are we there yet?\r\n'
+
+    '''
 
     return 'PRIVMSG %s :%s\r\n' % (channel, msg)
